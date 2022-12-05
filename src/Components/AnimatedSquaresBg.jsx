0ce58ecac18215c375483113c6bb60bf834d6acb
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-12-05 14:44:20 +0100

    Add animated squares bg component

---
+++ src/Components/AnimatedSquaresBg.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./animatedsquaresbg.css";
+
+const AnimatedSquaresBg = () => {
+  return (
+    <>
+      <div class="context">
+        <h1>Pure Css Animated Background</h1>
+      </div>
+
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default AnimatedSquaresBg;
--- src/Pages/Backgrounds.jsx
+++ src/Pages/Backgrounds.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import AnimatedColoursBg from "../Components/AnimatedColoursBg";
+import AnimatedSquaresBg from "../Components/AnimatedSquaresBg";
 
 const Backgrounds = () => {
   return (
-    <div>
+    <div className="m-4 p-4">
       <h1 className="flex flex-col text-pink-500 font-poppins">Inputs</h1>
       <p>Backgrounds with differents styles and designs</p>
 
@@ -19,6 +20,20 @@ const Backgrounds = () => {
           </a>
         </p>
         <AnimatedColoursBg />
+      </section>
+
+      <section className="flex flex-col">
+        <h2 className="m-2 text-purple-500">Animated background with squares</h2>
+        <p>
+          Based on{" "}
+          <a
+            href="https://alvarotrigo.com/blog/animated-backgrounds-css/"
+            alt="redirection vers le code de alvarotrigo "
+          >
+            alvaro trigo code
+          </a>
+        </p>
+        <AnimatedSquaresBg />
       </section>
     </div>
   );
