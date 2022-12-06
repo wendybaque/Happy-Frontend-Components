8c2aa43c716f51061ca434bbb04fea27493f9539
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-12-06 09:50:21 +0100

    Add heart beat in various component

---
+++ src/Components/HeartBeat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Components/heartbeat.css";
+
+const HeartBeat = () => {
+  return (
+    <div className='heart'>
+        <i className='fa fa-heart'></i>
+    </div>
+  )
+}
+
+export default HeartBeat
--- src/Pages/Various.jsx
+++ src/Pages/Various.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import HeartBeat from '../Components/HeartBeat'
 
 const Various = () => {
   return (
-    <div>
-      Various
+<div>
+      <div className="m-4 p-4 flex flex-col">
+        <h1 className="flex flex-col items-center text-l font-semibold text-pink-500 font-poppins">Various items...</h1>
+        <p className="flex flex-col items-center text-l m-4">Lots of items for fun !</p>
+
+        <section className="flex flex-col">
+          <h2 className="font-semibold text-purple-500">Heart Beat</h2>
+          <p className="pb-10">
+            Based on{" "}
+            <a href="https://www.instagram.com/p/Chyf73jjcjl/" alt="redirection vers le code de html_learninget & frontend_vishal7">
+              inspired by html_learninget & frontend_vishal7
+            </a>
+          </p>
+          <HeartBeat />
+        </section>
+
+      </div>
     </div>
   )
 }
