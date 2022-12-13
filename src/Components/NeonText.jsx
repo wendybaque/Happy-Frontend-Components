110690b192f0492377bd3c4fc5e41e945548901a
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-12-13 15:59:06 +0100

    Add neon text effect in texts components

---
+++ src/Components/NeonText.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./neontext.css";
+
+const NeonText = () => {
+  return (
+    <div>
+      <h2 className="neon-title">Hello World !</h2>
+    </div>
+  );
+};
+
+export default NeonText;
--- src/Pages/Texts.jsx
+++ src/Pages/Texts.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import NeonText from "../Components/NeonText";
 
 const Texts = () => {
   return (
-    <div>
-      Texts
-    </div>
-  )
-}
+    <div className="m-4 p-4 flex flex-col">
+      <h1 className="flex flex-col items-center text-l font-semibold text-pink-500 font-poppins">
+        Texts items...
+      </h1>
+      <p className="flex flex-col items-center text-l m-4">
+        Lots of texts effects for fun !
+      </p>
 
-export default Texts
+      <section className="flex flex-col">
+        <h2 className="font-semibold text-purple-500">Heart Beat</h2>
+        <p className="pb-10">
+          Based on{" "}
+          <a
+            href="https://www.instagram.com/p/Ce2mqa3pttD/"
+            alt="redirection vers le code de baby_wolf_codes"
+          >
+            inspired by baby_wolf_codes
+          </a>
+        </p>
+        <NeonText />
+      </section>
+    </div>
+  );
+};
+
+export default Texts;
