da2de4572e27ab76ec9dacf7f942c4230951fe62
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-01-03 08:42:54 +0100

    Add error 404 page

--- src/Pages/Buttons.jsx
+++ src/Pages/Buttons.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Buttons = () => {
   return (
-    <div>Buttons</div>
-  )
-}
+    <div className="m-4 p-4 flex flex-col">
+      <h1 className="flex flex-col items-center text-l font-semibold text-pink-500 font-poppins">
+        Buttons
+      </h1>
+      <p className="flex flex-col items-center text-l m-4">
+        Buttons with differents styles, controls and designs
+      </p>
 
-export default Buttons
+      <section className="flex flex-col w-full m-12">
+
+      </section>
+    </div>
+  );
+};
+
+export default Buttons;
--- src/Pages/Page404.jsx
+++ src/Pages/Page404.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./page404.css";
 
 const Page404 = () => {
-  return (
-    <div>Page404</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Page404
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  });
+  return (
+    <>
+      <div className="error-body">
+        <div className="error-wrap">
+          <div className="html"></div>
+          <div className="play">SORRY</div>
+          <div className="with">ERROR</div>
+          <div className="code">404</div>
+        </div>
+      </div>
+      <p className="error-p">Automatic redirection to homepage</p>
+    </>
+  );
+};
+
+export default Page404;
